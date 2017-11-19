7bb8fa3fd7817dfcfe655b164da0f110816b6198
Oliver Phillip Roer <oliver.roer@gmail.com>
2017-11-19 23:33:29 +0100

    init

---
+++ CoreUploadClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CoreUploadClient
+{
+    class Program
+    {
+        static int WriteError()
+        {
+            Console.Error.WriteLine("usage: TODO");
+            return -1;
+        }
+
+        static async Task UploadAsync(string endpoint)
+        {
+            
+
+            var client = new HttpClient();
+            int bufferSize = 1024 * 1024;
+            HttpContent content = new StreamContent(Console.OpenStandardInput(), bufferSize);
+            HttpResponseMessage response = await client.PostAsync(endpoint, content);
+            
+            if (response.IsSuccessStatusCode)
+            {
+
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            if (args == null || args.Length <= 0)
+            {
+                return WriteError();
+            }
+
+            string endpoint = args[0];
+
+            if(!Uri.IsWellFormedUriString(endpoint, UriKind.Absolute))
+            {
+                return WriteError();
+            }
+
+            UploadAsync(endpoint).Wait();
+            return 0;
+        }
+    }
+}
---
+++ MakeContent/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace MakeContent
+{
+    class Program
+    {
+        static int WriteError()
+        {
+            Console.Error.WriteLine("usage: TODO");
+            return -1;
+        }
+
+        static int Main(string[] args)
+        {
+            if (args == null || args.Length <= 0)
+            {
+                return WriteError();
+            }
+            
+            if (!ulong.TryParse(args[0], out ulong length))
+            {
+                return WriteError();
+            }
+
+            Stream stream = Console.OpenStandardOutput();
+            const int bufferSize = 1024 * 1024;
+            byte[] buffer = new byte[bufferSize];
+
+            int bufferIndex = 0;
+            void Write(char c)
+            {
+                buffer[bufferIndex++] = (byte)c;
+                if (bufferIndex >= buffer.Length)
+                {
+                    bufferIndex = 0;
+                    stream.Write(buffer, 0, buffer.Length);
+                    stream.Flush();
+                }
+            }
+
+            void Close()
+            {
+                if (bufferIndex > 0)
+                {
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+                stream.Close();
+            }
+            
+            {
+                char c = 'a';
+
+                for (ulong i = 0; i < length; ++i)
+                {
+
+                    Write(c);
+
+                    if (++c > 'z')
+                    {
+                        c = 'a';
+                    }
+                }
+            }
+            Close();
+
+            return 0;
+        }
+    }
+}
